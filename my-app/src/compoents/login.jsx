b4864f0bb11205350c366d40a06bdd4dd9157e10
DESKTOP-EJDVHOT\parkj <park.james1030@gmail.com>
2021-11-10 10:45:28 -0600

    created login and logout pages

---
+++ my-app/src/compoents/login.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useHistory } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import useForm from './useForm';
+import useLogin from './useLogin';
+import './login.css';
+
+const Login = () => {
+    const history = useHistory();
+    const send = useLogin();
+    
+    const { formValues, handleChange, handleSubmit } = useForm(() => login(formValues, send, history));
+
+    return (
+        <div class='body'>
+            <form onSubmit={handleSubmit} >
+                <div class="mb-3">
+                    <label for="usernameInput" class="form-label">Username</label>
+                    <input type="username" name="username" value={formValues.username} onChange={handleChange} class="form-control" id="usernameInput" aria-describedby="emailHelp" />
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" name="password" value={formValues.password} onChange={handleChange} class="form-control" id="exampleInputPassword1" />
+                </div>
+                <button class="login-button" type="submit" >Submit</button>
+                <p>Not registered? Sign up <Link to="/register">here.</Link></p>
+            </form>
+        </div>
+        )
+}
+
+export default Login;
+
+function login(values, send, history) {
+        send(values);
+        history.push("/");
+}
+
